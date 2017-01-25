1c59df4f5516b1ea0c54477b24f87e1eceeb31e0
Will <will@bluechilli.com>
2017-01-25 16:31:27 +1100

    first commit

---
+++ src/ChilliSource.Cloud.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChilliSource.Cloud.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
