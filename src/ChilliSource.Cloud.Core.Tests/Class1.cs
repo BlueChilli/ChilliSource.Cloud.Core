5d88a4c7ce710bbea915ee820c8c1e69b7ec805f
Will <will@bluechilli.com>
2017-04-03 16:37:51 +1000

    build tools

---
+++ src/ChilliSource.Cloud.Core.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace ChilliSource.Cloud.Core.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestFact()
+        {
+
+        }
+    }
+}
