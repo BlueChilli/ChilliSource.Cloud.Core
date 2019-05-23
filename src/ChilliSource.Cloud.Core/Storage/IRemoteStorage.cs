7e97952ecf55e7193ca6456446257115111063aa
Will <will@bluechilli.com>
2019-05-23 14:11:50 +1000

    RemoteStorage metadata

--- src/ChilliSource.Cloud.Core/Storage/IRemoteStorage.cs
+++ src/ChilliSource.Cloud.Core/Storage/IRemoteStorage.cs
@@ -14,6 +14,25 @@ namespace ChilliSource.Cloud.Core
         Task DeleteAsync(string fileToDelete, CancellationToken cancellationToken);
         Task<FileStorageResponse> GetContentAsync(string fileName, CancellationToken cancellationToken);
         Task<bool> ExistsAsync(string fileName, CancellationToken cancellationToken);
+        Task<IFileStorageMetadata> GetMetadataAsync(string fileName, CancellationToken cancellationToken);
+    }
+
+    public interface IFileStorageMetadata
+    {
+        DateTime LastModifiedUtc { get; }
+        long ContentLength { get; }
+        string CacheControl { get; }
+        string ContentEncoding { get; }
+        string ContentType { get; }
+    }
+
+    public class FileStorageMetadata : IFileStorageMetadata
+    {
+        public DateTime LastModifiedUtc { get; set; }
+        public long ContentLength { get; set; }
+        public string ContentType { get; set; }
+        public string CacheControl { get; set; }
+        public string ContentEncoding { get; set; }
     }
 
     public class FileStorageResponse
@@ -31,9 +50,22 @@ namespace ChilliSource.Cloud.Core
             return new FileStorageResponse(fileName, contentLength, contentType, stream);
         }
 
+        public static FileStorageResponse Create(string fileName, FileStorageMetadata metadata, Stream stream)
+        {
+            var response = new FileStorageResponse(fileName, metadata.ContentLength, metadata.ContentType, stream);
+            response.LastModifiedUtc = metadata.LastModifiedUtc;
+            response.CacheControl = metadata.CacheControl;
+            response.ContentEncoding = metadata.ContentEncoding;
+
+            return response;
+        }
+
         public string FileName { get; internal set; }
         public long ContentLength { get; internal set; }
         public string ContentType { get; internal set; }
         public Stream Stream { get; internal set; }
+        public DateTime LastModifiedUtc { get; internal set; }
+        public string CacheControl { get; internal set; }
+        public string ContentEncoding { get; internal set; }
     }
 }
