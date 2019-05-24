81326d45047a01a4f8441b4589a4c36a59239600
Will <will@bluechilli.com>
2019-05-24 14:46:34 +1000

    Local storage

---
+++ src/ChilliSource.Cloud.Core/Storage/LocalStorage/LocalStorageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChilliSource.Cloud.Core
+{
+    public class LocalStorageConfiguration
+    {
+        public string BasePath { get; set; }
+
+        internal LocalStorageConfiguration Clone()
+        {
+            var clone = (LocalStorageConfiguration)this.MemberwiseClone();
+            return clone;
+        }
+    }
+}
---
+++ src/ChilliSource.Cloud.Core/Storage/LocalStorage/LocalStorageProvider.cs
@@ -0,0 +1,144 @@
+﻿using ChilliSource.Cloud.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChilliSource.Cloud.Core
+{
+    public class LocalStorageProvider : IRemoteStorage
+    {
+        LocalStorageConfiguration _options;
+        public LocalStorageProvider(LocalStorageConfiguration options)
+        {
+            _options = options.Clone();
+
+            _options.BasePath = Path.GetFullPath(options.BasePath);
+        }
+
+        private string GetAbsolutePath(string file)
+        {
+            return Path.Combine(_options.BasePath, file.Replace("/", "\\"));
+        }
+
+        public Task DeleteAsync(string fileToDelete, CancellationToken cancellationToken)
+        {
+            var path = GetAbsolutePath(fileToDelete);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> ExistsAsync(string fileName, CancellationToken cancellationToken)
+        {
+            var path = GetAbsolutePath(fileName);
+            var exists = File.Exists(path);
+
+            return Task.FromResult(exists);
+        }
+
+        public async Task<FileStorageResponse> GetContentAsync(string fileName, CancellationToken cancellationToken)
+        {
+            var path = GetAbsolutePath(fileName);
+
+            var metadata = await this.GetMetadataAsync(fileName, cancellationToken);
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                Action<Stream> disposingAction = (s) =>
+                {
+                    s?.Dispose();
+                };
+
+                var readonlyStream = ReadOnlyStreamWrapper.Create(fileStream, disposingAction, fileStream.Length);
+                var response = FileStorageResponse.Create(metadata, readonlyStream);
+                return response;
+            }
+            catch
+            {
+                fileStream?.Dispose();
+
+                throw;
+            }
+        }
+
+        public async Task SaveAsync(Stream stream, string fileName, string contentType, CancellationToken cancellationToken)
+        {
+            await SaveAsync(stream, new FileStorageMetadataInfo()
+            {
+                FileName = fileName,
+                ContentType = contentType
+            }, cancellationToken);
+        }
+
+        public async Task SaveAsync(Stream stream, FileStorageMetadataInfo metadata, CancellationToken cancellationToken)
+        {
+            var path = GetAbsolutePath(metadata.FileName);
+            var directory = Path.GetDirectoryName(path);
+            Directory.CreateDirectory(directory);
+
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                await stream.CopyToAsync(fileStream, 4096, cancellationToken);
+                await fileStream.FlushAsync(cancellationToken);
+
+                fileStream.Close();
+            }
+
+            var metadataJsonContent = JsonConvert.SerializeObject(metadata);
+            using (var metaFile = new StreamWriter(GetMetadataJsonFilename(path), false, Encoding.UTF8))
+            {
+                await metaFile.WriteAsync(metadataJsonContent);
+                await metaFile.FlushAsync();
+
+                metaFile.Close();
+            }
+        }
+
+        private string GetMetadataJsonFilename(string path)
+        {
+            var ext = Path.GetExtension(path);
+            var metadataJsonFilename = Path.ChangeExtension(path, ext + ".meta");
+            return metadataJsonFilename;
+        }
+
+        public async Task<IFileStorageMetadataResponse> GetMetadataAsync(string fileName, CancellationToken cancellationToken)
+        {
+            var path = GetAbsolutePath(fileName);
+            var fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException($"File not found", fileName);
+            }
+
+            FileStorageMetadataInfo metadataJson = null;
+            using (var reader = new StreamReader(GetMetadataJsonFilename(path), Encoding.UTF8))
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                var jsonContent = await reader.ReadToEndAsync(); // cancellationToken not supported
+                metadataJson = JsonConvert.DeserializeObject<FileStorageMetadataInfo>(jsonContent) ?? new FileStorageMetadataInfo();
+            }
+
+            var metadata = new FileStorageMetadataResponse()
+            {
+                FileName = fileName,
+                CacheControl = metadataJson.CacheControl,
+                ContentDisposition = metadataJson.ContentDisposition,
+                ContentEncoding = metadataJson.ContentEncoding,
+                ContentLength = fileInfo.Length,
+                ContentType = metadataJson.ContentType,
+                LastModifiedUtc = fileInfo.LastWriteTimeUtc
+            };
+
+            return metadata;
+        }
+    }
+}
