ef8fa299a0e7b604cece097e11b745077c8325d3
drgrieve543 <drgrieve543@gmail.com>
2020-11-30 18:17:35 +1100

    google timezone helper

---
+++ src/ChilliSource.Cloud.Core/Infrastructure/Google/GoogleTimezone.cs
@@ -0,0 +1,52 @@
+﻿using ChilliSource.Core.Extensions;
+using RestSharp;
+using System;
+using System.Threading;
+
+namespace ChilliSource.Cloud.Core
+{
+
+    public class GoogleTimezone
+    {
+        public const string MapUri = "https://maps.googleapis.com/maps/api/timezone/json";
+
+        private string _apiKey { get; set; }
+        private int _delay { get; set; }
+
+        //If looping, set a short delay to avoid being rate limited. Delay is in milliseconds. 50 is fine.
+        public GoogleTimezone(string apikey, int delay = 0)
+        {
+            _apiKey = apikey;
+            _delay = delay;
+        }
+
+        /// <summary>
+        /// Return timezone for a lat, lng position
+        /// </summary>
+        public ServiceResult<GoogleTimeZone> Search(double latitude, double longitude)
+        {
+            if (_delay != 0) Thread.Sleep(_delay);
+
+            var client = new RestClient("https://maps.googleapis.com");
+            var request = new RestRequest("maps/api/timezone/json", Method.GET);
+            request.AddParameter("key", _apiKey);
+            request.AddParameter("location", $"{latitude},{longitude}");
+            request.AddParameter("timestamp", DateTime.UtcNow.ToUnixTime());
+            request.AddParameter("sensor", "false");
+            var response = client.Execute<GoogleTimeZone>(request);
+
+            return ServiceResult<GoogleTimeZone>.AsSuccess(response.Data);
+        }
+
+    }
+
+    public class GoogleTimeZone
+    {
+        public double DstOffset { get; set; }
+        public double RawOffset { get; set; }
+        public string Status { get; set; }
+        public string TimeZoneId { get; set; }
+        public string TimeZoneName { get; set; }
+    }
+
+}
