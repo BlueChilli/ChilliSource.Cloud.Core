64d2b098fa025b8776c6dd1572c65d2f88995d83
drgrieve543 <drgrieve543@gmail.com>
2021-08-18 12:07:52 +1000

    update packages

--- src/ChilliSource.Cloud.Core.Tests/Extensions/CsvHelperExtensionTests.cs
+++ src/ChilliSource.Cloud.Core.Tests/Extensions/CsvHelperExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace ChilliSource.Cloud.Core.Tests
         [Fact]
         public void ToCsv_FromListInt_ShouldReturnCorrectCsv()
         {
-            var result = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.ToCsvFile(new CsvHelper.Configuration.Configuration { ShouldQuote = (field, context) => false });
+            var result = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.ToCsvFile(new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture) { ShouldQuote = arg => false });
 
             Assert.Equal("1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n", result);
         }
