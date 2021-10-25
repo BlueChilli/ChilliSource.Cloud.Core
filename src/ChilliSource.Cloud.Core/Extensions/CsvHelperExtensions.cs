23e06db3dd0289980a97dd756fc703e4025bf6c4
drgrieve543 <drgrieve543@gmail.com>
2021-10-25 12:25:57 +1100

    fix csv helper

--- src/ChilliSource.Cloud.Core/Extensions/CsvHelperExtensions.cs
+++ src/ChilliSource.Cloud.Core/Extensions/CsvHelperExtensions.cs
@@ -53,12 +53,8 @@ namespace ChilliSource.Cloud.Core
         /// <summary>
         /// Converts a System.Collections.Generic.List&lt;T&gt; to comma delimiter string based on the CsvHelper.Configuration.CsvConfiguration.
         /// </summary>
-        /// <typeparam name="T">The type of the objects to convert.</typeparam>
-        /// <typeparam name="TClassMap">The type of the class map to register</typeparam>
-        /// <param name="items">A System.Collections.Generic.List&lt;T&gt;.</param>
-        /// <param name="config">CsvHelper.Configuration.CsvConfiguration</param>
-        /// <returns>Comma delimiter string.</returns>
-        public static string ToCsvFile<T, TClassMap>(this List<T> items, CsvConfiguration config) where TClassMap : ClassMap
+        /// <param name="setContext">Set context with passed in action</param>
+        public static string ToCsvFile<T>(this List<T> items, CsvConfiguration config, Action<CsvContext> setContext)
         {
             string csv = "";
 
@@ -67,7 +63,7 @@ namespace ChilliSource.Cloud.Core
                 using (var stringWriter = new StringWriter())
                 using (var writer = new CsvWriter(stringWriter, config))
                 {
-                    writer.Context.RegisterClassMap<TClassMap>();
+                    setContext(writer.Context);
                     writer.WriteRecords(items);
                     stringWriter.Flush();
 
