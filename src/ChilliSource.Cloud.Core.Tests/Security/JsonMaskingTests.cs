7b1ae67d7d5b05be27159c690dd95d3abd5df6c1
drgrieve543 <drgrieve543@gmail.com>
2022-11-04 10:11:51 +1100

    add fixed version of json masking

---
+++ src/ChilliSource.Cloud.Core.Tests/Security/JsonMaskingTests.cs
@@ -0,0 +1,34 @@
+﻿using ChilliSource.Cloud.Core.Security;
+using ChilliSource.Core.Extensions;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChilliSource.Cloud.Core.Tests
+{
+    public class JsonMaskingTests
+    {
+        private const string invalidJson = "{ \"name\": \"jim }";
+        private const string json = "{\"name\":\"jim\",\"password\":\"jimrocks\",\"age\":50,\"pins\":[1123,1234],\"secrets\":[{\"secret\":\"topsecret\"}]}";
+
+        [Fact]
+        public void JsonMaskingWorks()
+        {
+            var notMasked = invalidJson.MaskFields(new string[] { "password", "pin" }, "*****");
+            Assert.Equal(invalidJson, notMasked);
+
+            var notMasked2 = json.MaskFields(new string[] { }, "*****");
+            Assert.Equal(json, notMasked2);
+
+            var notMasked3 = json.MaskFields(new string[] { "jack", "jill" }, "*****");
+            Assert.Equal(json, notMasked3);
+
+            var isMasked = json.MaskFields(new string[] { "PASSWORD", "pins", "secret" }, "*****");
+            Assert.Equal("{\"name\":\"jim\",\"password\":\"*****\",\"age\":50,\"pins\":\"*****\",\"secrets\":[{\"secret\":\"*****\"}]}", isMasked);
+        }
+
+    }
+
+}
---
+++ src/ChilliSource.Cloud.Core/Security/JsonMasking.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChilliSource.Cloud.Core.Security
+{
+    /// <summary>
+    /// https://github.com/ThiagoBarradas/jsonmasking/blob/master/JsonMasking/JsonMasking.cs
+    /// </summary>
+    public static class JsonMasking
+    {
+        /// <summary>
+        /// Mask fields
+        /// </summary>
+        /// <param name="json">json to mask properties</param>
+        /// <param name="blacklist">insensitive property array</param>
+        /// <param name="mask">mask to replace property value</param>
+        /// <returns></returns>
+        public static string MaskFields(this string json, string[] blacklist, string mask)
+        {
+            if (string.IsNullOrWhiteSpace(json) == true)
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            if (blacklist == null)
+            {
+                throw new ArgumentNullException(nameof(blacklist));
+            }
+
+            if (blacklist.Any() == false)
+            {
+                return json;
+            }
+
+            try
+            {
+                var jsonObject = (JObject)JsonConvert.DeserializeObject(json);
+
+                MaskFieldsFromJToken(jsonObject, blacklist, mask);
+
+                return jsonObject.ToString(Formatting.None);
+            }
+            catch
+            {
+                return json;
+            }
+        }
+
+        /// <summary>
+        /// Mask fields from JToken
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="blacklist"></param>
+        /// <param name="mask"></param>
+        private static void MaskFieldsFromJToken(JToken token, string[] blacklist, string mask)
+        {
+            JContainer container = token as JContainer;
+            if (container == null)
+            {
+                return; // abort recursive
+            }
+
+            List<JToken> removeList = new List<JToken>();
+            foreach (JToken jtoken in container.Children())
+            {
+                var matching = false;
+                if (jtoken is JProperty prop)
+                {
+                    matching = blacklist.Any(item =>
+                    {
+                        return String.Equals(prop.Name, item, StringComparison.OrdinalIgnoreCase);
+                    });
+
+                    if (matching)
+                    {
+                        removeList.Add(jtoken);
+                    }
+                }
+
+                // call recursive 
+                if (!matching) MaskFieldsFromJToken(jtoken, blacklist, mask);
+            }
+
+            // replace 
+            foreach (JToken el in removeList)
+            {
+                var prop = (JProperty)el;
+                prop.Value = mask;
+            }
+        }
+
+    }
+}
