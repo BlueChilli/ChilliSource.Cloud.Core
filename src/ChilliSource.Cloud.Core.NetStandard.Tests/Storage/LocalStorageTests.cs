14d681727cedeb542feb79129fdc3fc3955949f4
drgrieve543 <drgrieve543@gmail.com>
2023-02-24 16:45:01 +1100

    presigned urls

---
+++ src/ChilliSource.Cloud.Core.NetStandard.Tests/Storage/LocalStorageTests.cs
@@ -0,0 +1,48 @@
+﻿using ChilliSource.Core.Extensions;
+using Moq;
+using System;
+using System.IO;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Xunit;
+
+namespace ChilliSource.Cloud.Core.NetStandard.Tests
+{
+    public class LocalStorageTests
+    {
+
+        [Fact]
+        public void TestAccessKey()
+        {
+            var remoteStorage = new LocalStorageProvider(new LocalStorageConfiguration()
+            {
+                BasePath = "./testlocalstorage"
+            });
+
+            IFileStorage storage = FileStorageFactory.Create(remoteStorage);
+
+            var filename = "myfilename.pdf";
+            var url = storage.GetPreSignedUrl(filename, TimeSpan.FromMinutes(5));
+
+            Assert.NotNull(url);
+            Assert.StartsWith(filename, url);
+
+            var accessKey = url.Substring(url.IndexOf('?') + 1);
+            Assert.NotNull(accessKey);
+            Assert.StartsWith("accesskey", accessKey);
+
+            accessKey = url.Substring(url.IndexOf('=') + 1);
+            Assert.NotNull(accessKey);
+
+            Assert.True(remoteStorage.IsValidPreSignedUrl(filename, accessKey));
+
+            url = storage.GetPreSignedUrl(filename, TimeSpan.FromMinutes(-5));
+            accessKey = url.Substring(url.IndexOf('=') + 1);
+            Assert.False(remoteStorage.IsValidPreSignedUrl(filename, accessKey));
+        }
+    }
+
+}
---
+++ src/ChilliSource.Cloud.Core.NetStandard.Tests/Storage/StorageTests.cs
@@ -0,0 +1,116 @@
+﻿using ChilliSource.Core.Extensions;
+using Moq;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChilliSource.Cloud.Core.NetStandard.Tests
+{
+    public class StorageTests
+    {
+        private readonly Mock<IRemoteStorage> _remoteStorage;
+        private readonly IFileStorage _fixture;
+
+        public StorageTests()
+        {
+            _remoteStorage = new Mock<IRemoteStorage>();
+            _fixture = new FileStorage(_remoteStorage.Object);
+        }
+
+        [Fact]
+        public async Task SaveAsync_ShouldSaveFile()
+        {
+            var stream = new MemoryStream();
+            var result = "test.txt";
+            StorageCommand command = new StorageCommand()
+            {
+                ContentType = "text/plain",
+                FileName = "test.txt"
+            };
+
+            command.SetStreamSource(stream, "txt");
+            _remoteStorage.Setup(x => x.SaveAsync(It.IsAny<Stream>(), command.FileName, command.ContentType, CancellationToken.None))
+            .Returns(Task<string>.FromResult<string>(result))
+            .Verifiable();
+
+            var res = await _fixture.SaveAsync(command);
+            _remoteStorage.Verify();
+            Assert.Equal(result, res);
+        }
+
+        [Fact]
+        public async Task SaveAsync_ShouldSaveFile2()
+        {
+            var stream = new MemoryStream();
+            var result = "test.txt";
+            StorageCommand command = new StorageCommand()
+            {
+                ContentType = "text/plain",
+                FileName = "test",
+                Extension = ".txt"
+            };
+
+            command.SetStreamSource(stream, ".txt");
+            _remoteStorage.Setup(x => x.SaveAsync(It.IsAny<Stream>(), result, command.ContentType, CancellationToken.None))
+            .Returns(Task<string>.FromResult<string>(result))
+            .Verifiable();
+
+            var res = await _fixture.SaveAsync(command);
+            _remoteStorage.Verify();
+            Assert.Equal(result, res);
+        }
+
+        [Fact]
+        public async Task SaveAsync_ShouldAppendFolderToFileNameIfProvided()
+        {
+            var stream = new MemoryStream();
+            var result = "Test/test.txt";
+            StorageCommand command = new StorageCommand()
+            {
+                ContentType = "text/plain",
+                FileName = "test.txt",
+                Folder = "Test"
+            };
+
+            command.SetStreamSource(stream, "txt");
+            _remoteStorage.Setup(x => x.SaveAsync(It.IsAny<Stream>(), result, command.ContentType, CancellationToken.None))
+            .Returns(Task<string>.FromResult<string>(result))
+            .Verifiable();
+
+            var res = await _fixture.SaveAsync(command);
+            _remoteStorage.Verify();
+            Assert.Equal(result, res);
+        }
+
+        [Fact]
+        public async Task SaveAsync_ShouldDetermineContentTypeFromExtensionIfNotProvided()
+        {
+            var _mimeMapping = new Mock<IMimeMapping>();
+            _mimeMapping.Setup(x => x.GetMimeType(It.IsAny<string>())).Returns("text/plain");
+
+            GlobalConfiguration.Instance.SetMimeMapping(_mimeMapping.Object);
+
+            var stream = new MemoryStream();
+            var result = "Test/test.txt";
+            StorageCommand command = new StorageCommand()
+            {
+                FileName = "test.txt",
+                Folder = "Test"
+            };
+
+            command.SetStreamSource(stream, "txt");
+            _remoteStorage.Setup(x => x.SaveAsync(It.IsAny<Stream>(), result, "text/plain", CancellationToken.None))
+            .Returns(Task<string>.FromResult<string>(result))
+            .Verifiable();
+
+            var res = await _fixture.SaveAsync(command);
+            _remoteStorage.Verify();
+            Assert.Equal(result, res);
+        }
+
+    }
+
+
+}
