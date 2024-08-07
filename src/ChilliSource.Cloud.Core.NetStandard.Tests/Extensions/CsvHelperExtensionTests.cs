56b343b0298c32dcc7c0639adeb591bbf4e9a824
drgrieve543 <drgrieve543@gmail.com>
2024-08-07 11:07:17 +1000

    update csvhelper version

--- src/ChilliSource.Cloud.Core.NetStandard.Tests/Extensions/CsvHelperExtensionTests.cs
+++ src/ChilliSource.Cloud.Core.NetStandard.Tests/Extensions/CsvHelperExtensionTests.cs
@@ -10,9 +10,14 @@ namespace ChilliSource.Cloud.Core.Tests
         [Fact]
         public void ToCsv_FromListInt_ShouldReturnCorrectCsv()
         {
-            var result = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }.ToCsvFile(new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture) { ShouldQuote = arg => false });
+            var data = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var result = data.ToCsvFile(new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture) { ShouldQuote = arg => false });
 
             Assert.Equal("1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n", result);
+
+            result = data.ToCsvFile();
+            Assert.Equal("\"1\"\r\n\"2\"\r\n\"3\"\r\n\"4\"\r\n\"5\"\r\n\"6\"\r\n\"7\"\r\n\"8\"\r\n\"9\"\r\n", result);
         }
 
     }
